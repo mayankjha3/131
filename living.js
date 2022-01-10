38dee9bcfe990b04a2e3e5fbef79cb7ee6718e9e
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2022-01-10 11:01:14 +0530

    Add files via upload

---
+++ living.js
@@ -0,0 +1,58 @@
+img = "";
+myStatus = "";
+
+objects = [];
+
+function preload(){
+    img = loadImage("living.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(380,380);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd" , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+
+}
+
+function draw(){
+    image( img, 0 , 0 , 380 , 380);
+
+    if(myStatus != ""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(img , gotResult);
+        for(i = 0; i < objects.length; i++){
+            console.log("stactus:object detected");
+            document.getElementById("status").innerHTML = "Status : Detecting Objects";
+            document.getElementById("number_of_objects").innerHTML = "number of objects :" + objects.length;
+            fill(r , g , b);
+            stroke(r , g , b);
+            noFill();
+            percent = floor(objects[i].confidence*100);
+            text( objects[i].label + " " + percent + " %", objects[i].x , objects[i].y);
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }  
+}
+
+function modelLoaded(){
+    console.log("model has loaded");
+
+    myStatus = true;
+    objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error , results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function back(){
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function living_room(){
+    window.location = "living.html"
+}
+
+function bedroom(){
+    window.location = "bedroom.html"
+}
+
+function tv(){
+    window.location = "tv.html" 
+}
+
+function bottles(){
+    window.location = "bottles.html";
+}
+
+function fruit(){
+    window.location = "fruit.html";
+}
